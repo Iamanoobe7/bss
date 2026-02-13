31fa42b2aa19678e1ff4030d12bc1ec070880874
Iamanoobe7 <135895198+Iamanoobe7@users.noreply.github.com>
2026-02-12 23:06:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -33868,6 +33868,9 @@ function BeeSwarmSimulator(DATA){
         }
 
         items.starEgg.amount=100
+        items.giftedMythicEgg.amount=100
+        items.treat.amount=1000000000000
+        items.ticket.amount=1000000000
         player.updateInventory()
 
         let tut=['Welcome to Bee Swarm Simulator!',5000,'Click the egg icon on the bar on','the left to open your inventory!',4000,'Click on the Basic Egg to select it!',4000,'Hover your mouse over a hive slot','and click to hatch it!',4000,'Collect pollen in fields with your bee!',4000,'After filling up your bag, stand near','your hive to convert the pollen into honey!',4000,'Use honey to buy more bee eggs and new tools!',5000,'Talk to bears and complete their quests!',3000,'Have fun!'],t=1000
@@ -34760,3 +34763,4 @@ function BeeSwarmSimulator(DATA){
     
 
 }
+
